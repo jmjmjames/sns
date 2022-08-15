f3cc0045b21047e71bccc39798024378cb0a1732
jmjmjames <chungjm0711@gmail.com>
2022-08-16 00:18:40 +0900

    feat: init project
    * add front_end code
    * add build gradle
    id "com.github.node-gradle.node" version "3.3.0" node base 코드를 gradle로 build할 때 추가하는 디펜던시
    * deploy yml

---
+++ src/test/java/com/jongmin/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongmin.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
