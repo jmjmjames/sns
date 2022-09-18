d072dc707c4b1106322011115f61ec88d9d9cf5c
jmjmjames <chungjm0711@gmail.com>
2022-09-18 11:29:59 +0900

    [Feat] Alarm 기능을 UserController에 추가 (#14)

--- src/main/java/com/jongmin/sns/controller/UserController.java
+++ src/main/java/com/jongmin/sns/controller/UserController.java
@@ -4,14 +4,16 @@ import com.jongmin.sns.dto.UserDto;
 import com.jongmin.sns.dto.request.user.UserJoinRequest;
 import com.jongmin.sns.dto.request.user.UserLoginRequest;
 import com.jongmin.sns.dto.response.Response;
+import com.jongmin.sns.dto.response.alarm.AlarmResponse;
 import com.jongmin.sns.dto.response.user.UserJoinResponse;
 import com.jongmin.sns.dto.response.user.UserLoginResponse;
+import com.jongmin.sns.dto.security.UserPrincipal;
 import com.jongmin.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
@@ -31,5 +34,12 @@ public class UserController {
         String token = userService.login(request.toDto());
         return Response.success(new UserLoginResponse(token));
     }
+
+    @GetMapping("/alarm")
+    public Response<Page<AlarmResponse>> alarm(Pageable pageable,
+                                               @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return Response.success(userService.alarmList(userPrincipal.getUsername(), pageable).map(AlarmResponse::fromDto));
+    }
+
 }
 
--- src/main/java/com/jongmin/sns/service/UserService.java
+++ src/main/java/com/jongmin/sns/service/UserService.java
@@ -1,13 +1,17 @@
 package com.jongmin.sns.service;
 
-import com.jongmin.sns.domain.User;
+import com.jongmin.sns.domain.user.User;
+import com.jongmin.sns.dto.AlarmDto;
 import com.jongmin.sns.dto.UserDto;
 import com.jongmin.sns.exception.ErrorCode;
 import com.jongmin.sns.exception.SnsApplicationException;
+import com.jongmin.sns.repository.AlarmRepository;
 import com.jongmin.sns.repository.UserRepository;
 import com.jongmin.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,12 +23,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+    private final AlarmRepository alarmRepository;
 
     @Value("${jwt.secret-key}")
     private String secretKey;
 
     @Value("${jwt.token-expired-time-ms}")
     private Long expiredTimeMs;
+
 
     @Transactional(readOnly = true)
     public UserDto loadUserByUserName(String userName) {
@@ -59,6 +65,15 @@ public class UserService {
         }
 
         return JwtTokenUtils.generateToken(dto.getUserName(), secretKey, expiredTimeMs);
+    }
+
+    public Page<AlarmDto> alarmList(String userName, Pageable pageable) {
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new SnsApplicationException(
+                        ErrorCode.USER_NOT_FOUND,
+                        String.format("%s not founded", userName)
+                ));
+        return alarmRepository.findAllByUser(user, pageable).map(AlarmDto::fromEntity);
     }
 
 }
